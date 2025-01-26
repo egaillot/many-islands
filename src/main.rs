a5a3d0e579dd74732e54bf20186fd3881f20db3c
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2025-01-26 13:56:02 +0100

    Print map with initial dot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+const SIZE_ORDER: u32 = 4;
+
 fn main() {
-    println!("Hello, world!");
+    const SIZE: usize = 2_u32.pow(SIZE_ORDER) as usize;
+
+    let mut map = [[0.0; SIZE + 1]; SIZE + 1];
+    map[SIZE / 2][SIZE / 2] = 1.0;
+
+    for row in map {
+        for cell in row {
+            let printable_cell = if cell > 0.5 { "• " }
+                                 else { "· " };
+            print!("{printable_cell}");
+        }
+        println!();
+    }
 }
