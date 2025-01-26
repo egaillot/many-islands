4015af2c4f81f37c8b1ae766a78175ae8dbba1cc
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2025-01-26 21:12:34 +0100

    [FIX] correctly compute middle / center elevation

--- src/main.rs
+++ src/main.rs
@@ -28,23 +28,32 @@ fn epsilon(rng: &mut ChaCha8Rng) -> f32 {
     0.3*(rng.gen::<f32>() - 0.5)
 }
 
-fn mutate_intermediary_cell(rng: &mut ChaCha8Rng, map: &mut [[f32; SIZE + 1]; SIZE + 1], row1: usize, col1: usize, row2: usize, col2: usize) {
-    let row = (row1 + row2) / 2;
-    let col = (col1 + col2) / 2;
+fn mutate_intermediary_cell(rng: &mut ChaCha8Rng, map: &mut [[f32; SIZE + 1]; SIZE + 1], points: Vec<[usize; 2]>) {
+    let mut row = 0;
+    let mut col = 0;
+    let mut elevation = 0.0;
 
-    if map[row][col] == 0.0 {
-        map[row][col] = (map[row1][col1] + map[row2][col2]) / 2.0 + epsilon(rng);
+    for [p_row, p_col] in &points {
+        row = row + p_row;
+        col = col + p_col;
+        elevation = elevation + map[*p_row][*p_col];
     }
+
+    row = row / points.len();
+    col = col / points.len();
+    elevation = elevation / points.len() as f32 + epsilon(rng);
+
+    if map[row][col] == 0.0 { map[row][col] = elevation; }
 }
 
 fn mutate_map(rng: &mut ChaCha8Rng, map: &mut [[f32; SIZE + 1]; SIZE + 1], delimiter: [usize; 4]) {
     let [top, left, bottom, right] = delimiter;
 
-    mutate_intermediary_cell(rng, map, top, left, top, right);
-    mutate_intermediary_cell(rng, map, bottom, left, bottom, right);
-    mutate_intermediary_cell(rng, map, top, left, bottom, left);
-    mutate_intermediary_cell(rng, map, top, right, bottom, right);
-    mutate_intermediary_cell(rng, map, top, left, bottom, right);
+    mutate_intermediary_cell(rng, map, vec![[top, left], [top, right]]);
+    mutate_intermediary_cell(rng, map, vec![[bottom, left], [bottom, right]]);
+    mutate_intermediary_cell(rng, map, vec![[top, left], [bottom, left]]);
+    mutate_intermediary_cell(rng, map, vec![[top, right], [bottom, right]]);
+    mutate_intermediary_cell(rng, map, vec![[top, left], [top, right], [bottom, left], [bottom, right]]);
 }
 
 fn init_map(submap_delimiters: Vec<[usize; 4]>) -> [[f32; SIZE + 1]; SIZE + 1] {
